46bed7ddc71858bb2bee6bb862b6105eed860e7f
Jialin Yang <yangjialin94@gmail.com>
2021-02-20 15:21:03 -0600

    finished tutorial #16

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import index
 
+app_name = "frontend"
 urlpatterns = [
-    path("", index, name="index"),
-    path("join/", index, name="index"),
-    path("create/", index, name="index"),
+    path("", index, name=""),
+    path("join/", index, name="join"),
+    path("create/", index, name="create"),
     path("room/<str:roomCode>", index, name="room"),
 ]
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "037690f438e5425d89e14bccad2aa093"
+CLIENT_SECRET = "86ff8c8d372b4741a07de4de9c706e47"
+REDIRECT_URI = "http:/127.0.0.1:8000/spotify/redirect"
